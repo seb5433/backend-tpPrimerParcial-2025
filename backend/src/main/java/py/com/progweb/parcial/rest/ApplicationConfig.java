b88f9f3e84416d5f2606a6adba3d39a7109f493a
Sebastian Alvarez <ca.sebastianlv@gmail.com>
2025-03-29 21:15:31 -0300

    Commit inicial

---
+++ backend/src/main/java/py/com/progweb/parcial/rest/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package py.com.progweb.parcial.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class ApplicationConfig extends Application {
+}
